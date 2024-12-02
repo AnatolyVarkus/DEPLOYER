dca3b6bd971fb71388b98b49c0109724f5da713f
AnatolyVarkus <164738391+AnatolyVarkus@users.noreply.github.com>
2024-12-03 02:37:03 +0700

    Update deployer.py

--- deployer.py
+++ deployer.py
@@ -44,6 +44,7 @@ provider = LiteClient(
 
 """Calculates the contract address based on the user_ids"""
 def calculate_contract_address(state_init) -> Address:
+    # Создаем ячейку кладем туда стейт инит
     return begin_cell().store_uint(4, 3).store_int(0, 8).store_uint(int.from_bytes(state_init.hash, "big"),
                                                                     256).end_cell().begin_parse().load_address()
 
@@ -51,11 +52,14 @@ def calculate_contract_address(state_init) -> Address:
 async def combine_deploy_messages(user_ids, wallet):
     messages = []
     for user_id in user_ids:
+        # Составляет init для деплоя (используем юзерайди для уникальности)
         collection_code = Builder.from_boc(config.COLLECTION)[0]
         data = begin_cell().store_int(user_id, 64).end_cell()
         init = StateInit(code=collection_code, data=data)
         address = calculate_contract_address(init.serialize())
+        # Обновляет в нашей бд адрес юзера, т.к. он появился 
         await update_user_address(user_id, address.to_str())
+        # Добавляем сообщение в блок
         messages.append(wallet.create_wallet_internal_message(destination=Address(address),
                                                               value=to_nano(0.05, "ton"),
                                                               state_init=init))
@@ -66,18 +70,25 @@ async def combine_deploy_messages(user_ids, wallet):
 """Deploys wallets and waits for seqno"""
 async def deploy_wallets(user_ids):
     await provider.connect()
+    # Наш деплоерский кош
     wallet = await WalletV4R2.from_mnemonic(provider=provider, mnemonics=mnemonics)
 
+    # В случае если заканчиваются бабки на коше, мы пополняем его с мейна
     if await wallet.get_balance() < config.MINIMUM_TON_BALANCE:
         await top_up_deploy_wallet(config.MINIMUM_TON_BALANCE)
 
+    # Получаем сообщения
     messages = await combine_deploy_messages(user_ids, wallet)
+    # Чекаем seqno
     url = f"https://tonapi.io/v2/blockchain/accounts/{wallet_address}/methods/seqno"
     async with httpx.AsyncClient() as client:
         response = await client.get(url)
         current_seqno = (response.json())["decoded"]["state"]
+    # Отправляем деплой сообщения
     await wallet.raw_transfer(messages)
     sleep_amount = 0
+
+    # Ждем изменение seqno, чтобы отправить следующую транзакцию 
     async with httpx.AsyncClient() as client:
         while current_seqno == ((await client.get(url)).json())["decoded"]["state"]:
             await asyncio.sleep(5)
@@ -92,10 +103,13 @@ async def deploy_wallets(user_ids):
 async def main_loop():
     logger.info("Starting")
     while TON_DURAK_IS_FOREVER:
+        # Основной цикл
         await asyncio.sleep(1)
         try:
             tx_amount = await get_deploy_tx_amount()
+            # Получает количество запросов на деплой из очереди
             if tx_amount > 4:
+                # Забирает 4 юзерайди из очереди
                 user_ids = await get_next_deploy_tx(4)
                 current_amount_taken = 4
             else:
@@ -103,7 +117,9 @@ async def main_loop():
                 current_amount_taken = tx_amount
             if len(user_ids) > 0:
                 try:
+                    # Деплоит коши
                     await deploy_wallets(user_ids)
+                    # Обновляет очередь
                     await deploy_queue_update(current_amount_taken)
                     logger.info(f"Deployed accounts: {user_ids}")
                 except Exception as e:
